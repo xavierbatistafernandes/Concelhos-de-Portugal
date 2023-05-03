cf4f8f6846363ac7df9f3636cbb01dbd5de7d197
xavierbatistafernandes <xavier.batista.fernandes@tecnico.ulisboa.pt>
2023-05-03 12:17:40 +0100

    debugging...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 function loadHurricane() {
-    window.location = "/example-02-07.html";
+    window.location = "~/example-02-07.html";
 }
 
 document.getElementById("btn-hurricane").addEventListener("click", loadHurricane);
