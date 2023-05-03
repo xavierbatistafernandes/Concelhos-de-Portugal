e4961dec8f5461c559399ca1554aab45973a389e
xavierbatistafernandes <xavier.batista.fernandes@tecnico.ulisboa.pt>
2023-05-03 23:25:58 +0100

    web mapping, first steps

--- js/index.js
+++ js/index.js
@@ -3,4 +3,9 @@ function loadHurricane() {
     window.location = "/webpage/example-02-07.html";
 }
 
+function loadCdP() {
+    window.location = "/webpage/cdp.html";
+}
+
 document.getElementById("btn-hurricane").addEventListener("click", loadHurricane);
+document.getElementById("btn-cdp").addEventListener("click", loadCdP);
